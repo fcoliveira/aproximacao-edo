bafbde18d6cf98ee1b97df5c968684899e101bfa
Fabio Oliveira <carvalho.fabio.oliveira@gmail.com>
2016-12-08 17:55:39 -0200

    arquivos desnecessarios removidos

--- euler_explicito.h
+++
@@ -1,9 +0,0 @@
-#ifndef EULER_EXPLICITO_H_INCLUDED
-#define EULER_EXPLICITO_H_INCLUDED
-
-    #include <stdio.h>
-
-    void euler_explicito (double t_minimo, double t_maximo, double (*f_edo)(double, double),
-                                                        int quantidade_passos, double condicao_contorno);
-
-#endif // EULER_EXPLICITO_H_INCLUDED
--- sol_analitica.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOL_ANALITICA_H_INCLUDED
-#define SOL_ANALITICA_H_INCLUDED
-
-    double sol_analitica (double t);
-    double (*analitico)(double);
-    void resolve_analitico (double (*analitico_f)(double), double h[ ]);
-
-#endif // SOL_ANALITICA_H_INCLUDED
